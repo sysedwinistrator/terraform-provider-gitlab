b2117e354fb7247e3b49ae8d67485c35df1ab6ad
Timo Furrer <tfurrer@gitlab.com>
2023-05-24 08:49:19 +0200

    Skip ldap acc tests for CE

--- internal/provider/sdk/resource_gitlab_group_ldap_link_test.go
+++ internal/provider/sdk/resource_gitlab_group_ldap_link_test.go
@@ -22,6 +22,8 @@ import (
 )
 
 func TestAccGitlabGroupLdapLink_SchemaMigration0_1(t *testing.T) {
+	testutil.SkipIfCE(t)
+
 	testGroup := testutil.CreateGroups(t, 1)[0]
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -61,6 +63,8 @@ func TestAccGitlabGroupLdapLink_SchemaMigration0_1(t *testing.T) {
 }
 
 func TestAccGitlabGroupLdapLink_basicCN(t *testing.T) {
+	testutil.SkipIfCE(t)
+
 	rInt := acctest.RandInt()
 	resourceName := "gitlab_group_ldap_link.foo"
 
@@ -78,8 +82,7 @@ func TestAccGitlabGroupLdapLink_basicCN(t *testing.T) {
 
 			// Create a group LDAP link as a developer (uses testAccGitlabGroupLdapLinkCreateConfig for Config)
 			{
-				SkipFunc: testutil.IsRunningInCE,
-				Config:   testAccGitlabGroupLdapLinkCreateConfig(rInt, &testLdapLink),
+				Config: testAccGitlabGroupLdapLinkCreateConfig(rInt, &testLdapLink),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGitlabGroupLdapLinkExists(resourceName, &ldapLink),
 					testAccCheckGitlabGroupLdapLinkAttributes(&ldapLink, &testAccGitlabGroupLdapLinkExpectedAttributes{
@@ -89,7 +92,6 @@ func TestAccGitlabGroupLdapLink_basicCN(t *testing.T) {
 
 			// Import the group LDAP link (re-uses testAccGitlabGroupLdapLinkCreateConfig for Config)
 			{
-				SkipFunc:          testutil.IsRunningInCE,
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
@@ -100,8 +102,7 @@ func TestAccGitlabGroupLdapLink_basicCN(t *testing.T) {
 
 			// Update the group LDAP link to change the access level (uses testAccGitlabGroupLdapLinkUpdateConfig for Config)
 			{
-				SkipFunc: testutil.IsRunningInCE,
-				Config:   testAccGitlabGroupLdapLinkUpdateConfig(rInt, &testLdapLink),
+				Config: testAccGitlabGroupLdapLinkUpdateConfig(rInt, &testLdapLink),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGitlabGroupLdapLinkExists(resourceName, &ldapLink),
 					testAccCheckGitlabGroupLdapLinkAttributes(&ldapLink, &testAccGitlabGroupLdapLinkExpectedAttributes{
@@ -113,6 +114,8 @@ func TestAccGitlabGroupLdapLink_basicCN(t *testing.T) {
 }
 
 func TestAccGitlabGroupLdapLink_basicFilter(t *testing.T) {
+	testutil.SkipIfCE(t)
+
 	resourceName := "gitlab_group_ldap_link.foo"
 
 	group := testutil.CreateGroups(t, 1)[0]
@@ -127,7 +130,6 @@ func TestAccGitlabGroupLdapLink_basicFilter(t *testing.T) {
 
 			// Create a group LDAP link using a valid filter
 			{
-				SkipFunc: testutil.IsRunningInCE,
 				Config: fmt.Sprintf(`resource "gitlab_group_ldap_link" "foo" {
 					group 		= "%d"
 					filter          = "(&(objectClass=person)(objectClass=user))"
@@ -138,7 +140,6 @@ func TestAccGitlabGroupLdapLink_basicFilter(t *testing.T) {
 				Check: testAccCheckGitlabGroupLdapLinkExists(resourceName, &ldapLink),
 			},
 			{
-				SkipFunc:          testutil.IsRunningInCE,
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
@@ -151,6 +152,8 @@ func TestAccGitlabGroupLdapLink_basicFilter(t *testing.T) {
 }
 
 func TestAccGitlabGroupLdapLink_conflictingArguments(t *testing.T) {
+	testutil.SkipIfCE(t)
+
 	group := testutil.CreateGroups(t, 1)[0]
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -161,7 +164,6 @@ func TestAccGitlabGroupLdapLink_conflictingArguments(t *testing.T) {
 			// Create a group LDAP link using conflicting arguments
 			// ensure both conflict errors are printed appropriately.
 			{
-				SkipFunc: testutil.IsRunningInCE,
 				Config: fmt.Sprintf(`resource "gitlab_group_ldap_link" "foo" {
 					group 		= "%d"
 					cn              = "default"
@@ -172,7 +174,6 @@ func TestAccGitlabGroupLdapLink_conflictingArguments(t *testing.T) {
 				ExpectError: regexp.MustCompile(regexp.QuoteMeta(`"cn": conflicts with filter`)),
 			},
 			{
-				SkipFunc: testutil.IsRunningInCE,
 				Config: fmt.Sprintf(`resource "gitlab_group_ldap_link" "foo" {
 					group 		= "%d"
 					cn              = "default"
