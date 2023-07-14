eb4cc5b8d4682cc739100cda14bd3499bbe021cd
arukiidou <arukiidou@yahoo.co.jp>
2023-07-14 06:23:50 +0000

    Docs: Add gitlab_group_badge usage guide with placeholders

--- examples/resources/gitlab_group_badge/resource.tf
+++ examples/resources/gitlab_group_badge/resource.tf
@@ -7,3 +7,24 @@ resource "gitlab_group_badge" "example" {
   link_url  = "https://example.com/badge-123"
   image_url = "https://example.com/badge-123.svg"
 }
+
+# Pipeline status badges with placeholders will be enabled for each project
+resource "gitlab_group_badge" "gitlab_pipeline" {
+  group     = gitlab_group.foo.id
+  link_url  = "https://gitlab.example.com/%%{project_path}/-/jobs/artifacts/main/browse"
+  image_url = "https://gitlab.example.com/%%{project_path}/badges/%%{default_branch}/pipeline.svg"
+}
+
+# Test coverage report badges with placeholders will be enabled for each project
+resource "gitlab_group_badge" "gitlab_coverage" {
+  group     = gitlab_group.foo.id
+  link_url  = "https://gitlab.example.com/%%{project_path}/-/jobs"
+  image_url = "https://gitlab.example.com/%%{project_path}/badges/%%{default_branch}/coverage.svg"
+}
+
+# Latest release badges with placeholders will be enabled for each project
+resource "gitlab_group_badge" "gitlab_release" {
+  group     = gitlab_group.foo.id
+  link_url  = "https://gitlab.example.com/%%{project_path}/-/releases"
+  image_url = "https://gitlab.example.com/%%{project_path}/-/badges/release.svg"
+}
